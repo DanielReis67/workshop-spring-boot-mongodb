d949555528954b70c1ca80ef383414922d3cd992
DanielReis67 <daniel.reisoliveira67@gmail.com>
2020-02-06 13:59:25 -0300

    Referenciando os posts do usuario

--- src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
@@ -44,6 +44,10 @@ public class Instantiation implements CommandLineRunner{
 				
 		//Salvando os dados de posts
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
+		
 	}
 
 }
